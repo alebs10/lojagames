58a05f3e15c2e5ac6092aaaa5b58dcd8baf94e11
alebs10 <alexandre2002.ab@gmail.com>
2022-05-11 15:32:29 -0300

    Classe Security inserida.

--- src/main/java/com/generation/lojadegames/model/Produtos.java
+++ src/main/java/com/generation/lojadegames/model/Produtos.java
@@ -51,6 +51,18 @@ public class Produtos {
 	@ManyToOne
 	@JsonIgnoreProperties("produtos")
 	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Usuario usuario;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	public Long getId() {
 		return id;
